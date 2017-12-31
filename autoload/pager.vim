lucc/nvimpager

commit c5181a1bc66db8698fb76d0aeb73b4787c028422

Start pager mode for every new buffer

autoload/pager.vim
@@ -20,7 +20,7 @@ function! pager#prepare_pager() abort
   call s:detect_file_type()
   call s:set_options()
   call s:set_maps()
-  autocmd NvimPager VimEnter * call s:pager()
+  autocmd NvimPager BufWinEnter * call s:pager()
 endfunction
 
 " Set up an VimEnter autocmd to print the files to stdout with highlighting.

commit ca711f1bf131c41e45a32b72f03ea326c64777f9

Fix pager mode for initial buffer
This fixes a regression introduced in
c5181a1bc66db8698fb76d0aeb73b4787c028422.  It seems that BufWinEnter was
not fired for the initial buffer.

autoload/pager.vim
@@ -20,7 +20,7 @@ function! pager#prepare_pager() abort
   call s:detect_file_type()
   call s:set_options()
   call s:set_maps()
-  autocmd NvimPager BufWinEnter * call s:pager()
+  autocmd NvimPager BufWinEnter,VimEnter * call s:pager()
 endfunction
 
 " Set up an VimEnter autocmd to print the files to stdout with highlighting.