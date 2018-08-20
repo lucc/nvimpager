cd6917f6e5b85aa6fd076dee6dd25dc47f5e5c46
Lucas Hoffmann <lucc@posteo.de>
2018-08-21 00:00:13 +0200

    Add tests for diff and modeline
    The diff filtype plugin already unsets 'modeline' but that is only
    triggered for pure diff buffers.  Git log output is highlighted
    differently.

--- test/nvimpager_spec.lua
+++ test/nvimpager_spec.lua
@@ -159,22 +159,44 @@ describe("cat mode", function()
     assert.equal('', output)
   end)
 
-  pending("highlights files even after mode line files", function()
-    local output = run("./nvimpager -c test/fixtures/conceal.tex " ..
-		       "test/fixtures/makefile " ..
-		       "--cmd \"let g:tex_flavor='latex'\"")
-    local expected = read("test/fixtures/conceal.tex.ansi") ..
-                     read("test/fixtures/makefile.ansi")
-    assert.equal(expected, output)
-  end)
+  describe("with modeline", function()
+    pending("highlights files even after mode line files", function()
+      local output = run("./nvimpager -c test/fixtures/conceal.tex " ..
+			 "test/fixtures/makefile " ..
+			 "--cmd \"let g:tex_flavor='latex'\"")
+      local expected = read("test/fixtures/conceal.tex.ansi") ..
+		       read("test/fixtures/makefile.ansi")
+      assert.equal(expected, output)
+    end)
 
-  pending("honors mode lines in later files", function()
-    local output = run("./nvimpager -c test/fixtures/makefile " ..
-		       "test/fixtures/conceal.tex " ..
-		       "--cmd \"let g:tex_flavor='latex'\"")
-    local expected = read("test/fixtures/makefile.ansi") ..
-                     read("test/fixtures/conceal.tex.ansi")
-    assert.equal(expected, output)
+    pending("honors mode lines in later files", function()
+      local output = run("./nvimpager -c test/fixtures/makefile " ..
+			 "test/fixtures/conceal.tex " ..
+			 "--cmd \"let g:tex_flavor='latex'\"")
+      local expected = read("test/fixtures/makefile.ansi") ..
+		       read("test/fixtures/conceal.tex.ansi")
+      assert.equal(expected, output)
+    end)
+
+    it("ignores mode lines in diffs", function()
+      local output = run("./nvimpager -c test/fixtures/diff-modeline 2>&1")
+      local expected = read("test/fixtures/diff-modeline.ansi")
+      assert.equal(expected, output)
+    end)
+
+    it("ignores mode lines in git diffs", function()
+      local output = run("test/fixtures/bin/git ./nvimpager -c " ..
+			 "test/fixtures/diff-modeline 2>&1")
+      local expected = read("test/fixtures/diff-modeline.ansi")
+      assert.equal(expected, output)
+    end)
+
+    pending("ignores mode lines in git log diffs", function()
+      local output = run("test/fixtures/bin/git ./nvimpager -c " ..
+			 "test/fixtures/git-log 2>&1")
+      local expected = read("test/fixtures/git-log.ansi")
+      assert.equal(expected, output)
+    end)
   end)
 
   describe("conceals", function()
