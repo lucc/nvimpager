a7634f01eb1c22b64d73147dad484fdb170ff277
Lucas Hoffmann <lucc@posteo.de>
2018-12-01 13:55:40 +0100

    Add some new mappings

--- lua/nvimpager.lua
+++ lua/nvimpager.lua
@@ -312,11 +312,14 @@ end
 -- Set up mappings to make nvim behave a little more like a pager.
 local function set_maps()
   nvim.nvim_command('nnoremap q :quitall!<CR>')
+  nvim.nvim_command('vnoremap q :quitall!<CR>')
   nvim.nvim_command('nnoremap <Space> <PageDown>')
   nvim.nvim_command('nnoremap <S-Space> <PageUp>')
   nvim.nvim_command('nnoremap g gg')
   nvim.nvim_command('nnoremap <Up> <C-Y>')
   nvim.nvim_command('nnoremap <Down> <C-E>')
+  nvim.nvim_command('nnoremap k <C-Y>')
+  nvim.nvim_command('nnoremap j <C-E>')
 end
 
 -- Unset all mappings set in set_maps().
