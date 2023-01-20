ea6eeab25ce410d90b47cd850e280cc330b4bf91
Lucas Hoffmann <lucc@posteo.de>
2023-01-20 10:16:36 +0100

    nix: move most let bindings to the top

--- flake.nix
+++ flake.nix
@@ -8,40 +8,16 @@
     neovim.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, neovim, ... }: {
-    overlays.default = final: prev: {
-      nvimpager =
-        let
-          inherit (builtins) head match readFile;
-          version = head (match ".*version=([0-9.]*)\n.*" (readFile ./nvimpager))
-            + "-dev-${toString self.sourceInfo.lastModifiedDate}";
-        in prev.nvimpager.overrideAttrs (oa: {
-          inherit version;
-          src = ./.;
-          buildFlags = oa.buildFlags ++ [ "VERSION=${version}" ];
-          checkPhase = ''
-            runHook preCheck
-            script -ec "busted --output TAP --lpath './?.lua' --filter-out 'handles man' test"
-            runHook postCheck
-          '';
-        });
-    };
-  }
-  // (let
-    filter = builtins.filter (s: !nixpkgs.lib.strings.hasSuffix "-darwin" s);
-    inherit (flake-utils.lib) eachSystem defaultSystems;
-  in eachSystem (filter defaultSystems) (system:
+  outputs = { self, nixpkgs, flake-utils, neovim, ... }:
   let
-    stable = import nixpkgs { overlays = [ self.overlays.default ]; inherit system; };
-    nightly = import nixpkgs { overlays = [ neovim.overlay self.overlays.default ]; inherit system; };
+    inherit (builtins) filter head match readFile;
+    inherit (flake-utils.lib) eachSystem defaultSystems;
+    version = head (match ".*version=([0-9.]*)\n.*" (readFile ./nvimpager))
+              + "-dev-${toString self.sourceInfo.lastModifiedDate}";
+    withoutDarwin = filter (s: !nixpkgs.lib.strings.hasSuffix "-darwin" s);
     mkShell = pkgs: pkgs.mkShell {
       inputsFrom = [ pkgs.nvimpager ];
-      packages = with pkgs; [
-        lua51Packages.luacov
-        git
-        tmux
-        hyperfine
-      ];
+      packages = with pkgs; [ lua51Packages.luacov git tmux hyperfine ];
       shellHook = ''
         # to find nvimpager lua code in the current dir
         export LUA_PATH=./?.lua''${LUA_PATH:+\;}$LUA_PATH
@@ -51,11 +27,28 @@
         nvim --version | head -n 1
         '';
       };
+  in ({
+    overlays.default = final: prev: {
+      nvimpager = prev.nvimpager.overrideAttrs (oa: {
+        inherit version;
+        src = ./.;
+        buildFlags = oa.buildFlags ++ [ "VERSION=${version}" ];
+        checkPhase = ''
+          runHook preCheck
+          script -ec "busted --output TAP --lpath './?.lua' --filter-out 'handles man' test"
+          runHook postCheck
+        '';
+      });
+    };
+  } // (eachSystem (withoutDarwin defaultSystems) (system:
+  let
+    stable = import nixpkgs { overlays = [ self.overlays.default ]; inherit system; };
+    nightly = import nixpkgs { overlays = [ neovim.overlay self.overlays.default ]; inherit system; };
   in {
     apps.default = flake-utils.lib.mkApp { drv = stable.nvimpager; };
     devShells.default = mkShell stable;
     devShells.nightly = mkShell nightly;
     packages.default = stable.nvimpager;
     packages.nightly = nightly.nvimpager;
-  }));
+  })));
 }
