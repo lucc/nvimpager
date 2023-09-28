2806e006741b3e486c697a8761b5d6025b4087dd
Lucas Hoffmann <lucc@posteo.de>
2023-09-28 08:24:16 +0200

    Add test to test new custom test framework
    The test framework should be able to report errors from the neovim
    assert functions to busted.

---
+++ test/meta_test.lua
@@ -0,0 +1,1 @@
+vim.fn.assert_report("this is an error")
--- test/native_spec.lua
+++ test/native_spec.lua
@@ -7,21 +7,38 @@ local describe, it, assert = describe, it, assert  -- luacheck: ignore
 local helpers = require("test/helpers")
 local env_var = "NVIMPAGER_NATIVE_OUT_FILE"
 
+local function run_test_file(filename, outfile)
+  helpers.run(
+    env_var .. "=" .. outfile ..
+    " ./nvimpager -p -- -c 'so %' "..
+    "-c 'cal writefile(v:errors, $" ..
+    env_var .. ")' -c 'qa!' " .. filename
+  )
+  return helpers.read(outfile)
+end
+local function assert_test_file(filename, outfile)
+  local output = run_test_file(filename, outfile)
+  assert.equal("", output)
+end
+
 describe("native", function()
+
+  -- tests for the custom test framework of this file
+  describe("test framework", function()
+    it("reports assert_* failures from neovim", function()
+      local out = os.tmpname()
+      finally(function() os.remove(out) end)
+      local output = run_test_file("test/meta_test.lua", out)
+      assert.not_nil(output:find("this is an error"))
+    end)
+  end)
 
   describe("pager mode", function()
 
     it("loads the nvimpager table", function()
       local out = os.tmpname()
       finally(function() os.remove(out) end)
-      helpers.run(
-	env_var .. "=" .. out ..
-	" ./nvimpager -p -- -c 'so %' "..
-	"-c 'cal writefile(v:errors, $" ..
-	env_var .. ")' -c 'qa!' test/first_test.lua"
-      )
-      local output = helpers.read(out)
-      assert.equal("", output)
+      assert_test_file("test/first_test.lua", out)
     end)
 
   end)
